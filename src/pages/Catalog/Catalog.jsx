9d1e802eeeaa91c7aee89e0239332038e55f188d
mominovDev <mardonmominov6@gmail.com>
2025-02-11 15:01:41 +0500

    Catalog page changed by Mardon

--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -5,12 +5,12 @@ const Catalog = () => {
     <>
       <sectiion className="catalog">
         <div className="container catalog-container">
-          <h1 data-aos="fade-left" className="catalog-container__title">{`Mahsulotlar ro'yhati`} <a href="/">yuklash<i className='bx bx-down-arrow-alt'></i></a></h1>
+          <h1 data-aos="fade-left" className="catalog-container__title">{`Список продуктов`} <a href="/images/ahmedov_katalog.pdf">yuklash<i className='bx bx-down-arrow-alt'></i></a></h1>
           <div data-aos="fade-up" className="catalog-container__wrapper">
-            <h1 className="catalog-container__wrapper__title">Muzqaymoqlar</h1>
+            <h1 className="catalog-container__wrapper__title">Мороженое</h1>
             <div data-aos="fade-up" className="catalog-container__wrapper__box">
               <h2 className="catalog-container__wrapper__box__subtitle">
-                Kreasy Max
+              Кризи Макс
               </h2>
               <div data-aos="fade-up" className="catalog-container__wrapper__box__row">
                 <a
@@ -18,63 +18,63 @@ const Catalog = () => {
                   className="catalog-container__wrapper__box__row__item"
                 >
                   <img src="images/kreasymaxberry.png" alt="iceCream" />
-                  <p>Kreasy Max Berry</p>
+                  <p>Кризи Макс Berry</p>
                 </a>
                 <a
                   href="/catalog"
                   className="catalog-container__wrapper__box__row__item"
                 >
                   <img src="images/kreasymaxberry.png" alt="iceCream" />
-                  <p>Kreasy Max Berry</p>
+                  <p>Кризи Макс Berry</p>
                 </a>
                 <a
                   href="/catalog"
                   className="catalog-container__wrapper__box__row__item"
                 >
                   <img src="images/kreasymaxberry.png" alt="iceCream" />
-                  <p>Kreasy Max Berry</p>
+                  <p>Кризи Макс Berry</p>
                 </a>
                 <a
                   href="/catalog"
                   className="catalog-container__wrapper__box__row__item"
                 >
                   <img src="images/kreasymaxberry.png" alt="iceCream" />
-                  <p>Kreasy Max Berry</p>
+                  <p>Кризи Макс Berry</p>
                 </a>
                 <a
                   href="/catalog"
                   className="catalog-container__wrapper__box__row__item"
                 >
                   <img src="images/kreasymaxberry.png" alt="iceCream" />
-                  <p>Kreasy Max Berry</p>
+                  <p>Кризи Макс Berry</p>
                 </a>
                 <a
                   href="/catalog"
                   className="catalog-container__wrapper__box__row__item"
                 >
                   <img src="images/kreasymaxberry.png" alt="iceCream" />
-                  <p>Kreasy Max Berry</p>
+                  <p>Кризи Макс Berry</p>
                 </a>
                 <a
                   href="/catalog"
                   className="catalog-container__wrapper__box__row__item"
                 >
                   <img src="images/kreasymaxberry.png" alt="iceCream" />
-                  <p>Kreasy Max Berry</p>
+                  <p>Кризи Макс Berry</p>
                 </a>
                 <a
                   href="/catalog"
                   className="catalog-container__wrapper__box__row__item"
                 >
                   <img src="images/kreasymaxberry.png" alt="iceCream" />
-                  <p>Kreasy Max Berry</p>
+                  <p>Кризи Макс Berry</p>
                 </a>
                 <a
                   href="/catalog"
                   className="catalog-container__wrapper__box__row__item"
                 >
                   <img src="images/kreasymaxberry.png" alt="iceCream" />
-                  <p>Kreasy Max Berry</p>
+                  <p>Кризи Макс Berry</p>
                 </a>
               </div>
             </div>
